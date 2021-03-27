a94be2b3d485c193ed950380a59f2e8847898080
ariffiansyah <80112480+ariffiansyah@users.noreply.github.com>
2021-03-27 18:50:46 +0700

    Add files via upload

---
+++ pertemuan5/test.php
@@ -0,0 +1,12 @@
+<html> 
+<head> 
+<title>Penggunaan For</title> 
+</head> 
+<body> 
+<?Php 
+ Hello, this is a test file. 
+There are three lines here. 
+This is the last line.  
+ ?> 
+</body> 
+</html> 
